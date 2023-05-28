8993a8a1d24e02acea6e68770cb19f9eb5f9ec4f
Rafael Cavalcante <rafacavalcante081@gmail.com>
2023-05-28 19:58:49 -0300

    home page is done

--- src/pages/home/homeController.js
+++ src/pages/home/homeController.js
@@ -1,8 +1,11 @@
 angular
   .module("appExemplo")
-  .controller("homeController", function ($rootScope, $scope) {
+  .controller("homeController", function ($rootScope, $scope, $location) {
     // Modificando a variável global através do escopo do controller
     $rootScope.isLoginPageGeneral = false;
-    $scope.projectList = "Project Listing"
     
+    $scope.projectList = "Project Listing";
+    $scope.setPage = function(){
+        $location.path("projects");
+    }    
   });
