fbea356b4973d1e1eaaa72264c39f2ee72275041
Rafael Cavalcante <rafacavalcante081@gmail.com>
2023-05-27 22:25:44 -0300

    feat: starting home page

--- src/pages/home/homeController.js
+++ src/pages/home/homeController.js
@@ -1,6 +1,8 @@
 angular
   .module("appExemplo")
-  .controller("homeController", function ($rootScope) {
+  .controller("homeController", function ($rootScope, $scope) {
     // Modificando a variável global através do escopo do controller
     $rootScope.isLoginPageGeneral = false;
+    $scope.projectList = "Project Listing"
+    
   });
