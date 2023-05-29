99db18db463de4a416493a7a7d3728300ddae888
JoseDavi <jose.davi.souza@ccc.ufcg.edu.br>
2023-05-29 15:50:10 -0300

    Login page

--- src/pages/login/loginController.js
+++ src/pages/login/loginController.js
@@ -1,7 +1,30 @@
 angular.module("appExemplo").controller("loginController", function ($scope) {
   $scope.showLogin = true;
+  $scope.username = "";
+  $scope.password = "";
+  $scope.type = "student";
+  $scope.user = {
+    type: "",
+    fullname: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    institution: "",
+    occupation: "",
+    formation: "",
+  };
   $scope.login = function () {
-    console.log("login");
+    console.log(
+      "login: ",
+      "user",
+      $scope.username,
+      "password",
+      $scope.password
+    );
     //login
+  };
+  $scope.register = function () {
+    $scope.user.type = $scope.type;
+    console.log($scope.user);
   };
 });
