371c6b9061ff0550a814e242328ca0ca08d12ea9
Rafael Cavalcante <rafacavalcante081@gmail.com>
2023-06-05 10:40:33 -0300

    starting pagination, fix bug

--- src/pages/projects/projectListing/projectListing.js
+++ src/pages/projects/projectListing/projectListing.js
@@ -141,7 +141,33 @@ angular.module("appExemplo").controller("projectListingController", function ($s
         },    
     ];
     $scope.pages = [ 5, 10, 15, 20, 25];
-    $scope.selectedPage = $scope.pages[4]
+
+    $scope.selectedPage = $scope.pages[0];
+    $scope.pageSize = $scope.selectedPage;
+    $scope.currentPage = 1;
+    $scope.totalPages = Math.ceil($scope.projects.length / $scope.pageSize);
+
+    $scope.paginateProjects = function(){
+        var startIndex = ($scope.currentPage - 1) * $scope.pageSize;
+        var endIndex = startIndex + $scope.pageSize;
+        $scope.paginateProjects = $scope.projects.slice(startIndex, endIndex);
+    }
+
+    $scope.previousPage = function(){
+        if($scope.currentPage > 1) {
+            $scope.currentPage--;
+            $scope.paginateProjects();
+        }
+    }
+
+    $scope.nextPage = function(){
+        if($scope.currentPage < $scope.totalPages) {
+            $scope.currentPage++;
+            $scope.paginateProjects();
+        }
+    }
+
+    $scope.paginateProjects($scope.currentPage);
 }).filter('limitarTamanho', function(){
     return function(input, tamanho) {
         if(input.length > tamanho){
