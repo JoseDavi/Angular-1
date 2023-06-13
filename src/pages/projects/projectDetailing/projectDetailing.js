3509502cd920a1af2fc43a6cd39139476980477a
unknown <rafacavalcante081@gmail.com>
2023-06-13 10:10:13 -0300

    HTML began and JS simplified, todo css

--- src/pages/projects/projectDetailing/projectDetailing.js
+++ src/pages/projects/projectDetailing/projectDetailing.js
@@ -1,3 +1,32 @@
-angular.module("appExemplo").controller("projectDetailingController", function () {
-    
+angular.module("appExemplo").controller("projectDetailingController", function ($scope) {
+    $scope.nameProject = "Name of Project";
+    $scope.id = 0002;
+    $scope.description = "Lorem Ipsum dolor sit amet, consectetur adipis, sed do eiusmod tempor incididunt ut labore et dol, itaque, sed do eiusmod tempor incididunt, ullamcorper itaque";
+    $scope.backButton = function() {
+        window.history.back();
+    }
+    $scope.projectCoordinator = "luisthiago";
+    $scope.students = [
+        {
+            name: "davigomes",
+            class: "Júnior"
+        },
+        {
+            name: "esteniosouza",
+            class: "Pleno"
+        },
+        {
+            name: "lucassantos",
+            class: "Pleno"
+        },
+        {
+            name: "gabrieloliveira",
+            class: "Sênior"
+        }
+    ]
+    $scope.upload = new Date(2022,17,7)
+    $scope.comments = ""
+    $scope.typeDocument = "Document - Any Generic Format"
+    $scope.format = "pdf"
+    $scope.fileSize = '820Kb'
 });
